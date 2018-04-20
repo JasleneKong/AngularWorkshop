c0b4bb4089adbca241384b8af11e0d6f2d965309
JasleneKong <jas_xueping@hotmail.com>
2018-04-20 11:07:54 +0800

    update Joke 1

--- src/app/jokelist.component.ts
+++ src/app/jokelist.component.ts
@@ -1,7 +1,7 @@
 
 import { Component } from '@angular/core';
 
-import {Joke } from './joke.component'
+import {Joke } from './joke.component';
 
 @Component({
     selector: 'joke-list',
@@ -15,7 +15,7 @@ export class JokeListComponent
    jokes: Joke[];
    constructor() {
        this.jokes = [
-           new Joke('Joke1', 'Hello 1'),
+           new Joke('Joke1', 'Me 1'),
            new Joke('Joke2', 'Hello 2'),
            new Joke('Joke3', 'Hello 3')
        ];
