c3469109a79a7dbd11edb51241f2e13291582fd1
JasleneKong <jas_xueping@hotmail.com>
2018-04-20 14:06:08 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
+import { JokeListComponent } from './joke-list/joke-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 
-import { JokeComponent } from './joke.component';
+import { JokeComponent } from './joke/joke.component';
 
-import { JokeListComponent } from './jokelist.component';
 import { JokeFormComponent } from './joke-form/joke-form.component';
 
 
