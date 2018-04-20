b4e3f5e43dbcb97fd81fefee535bee9235466ca9
JasleneKong <jas_xueping@hotmail.com>
2018-04-20 11:14:18 +0800

    update112

--- src/app/jokelist.component.ts
+++ src/app/jokelist.component.ts
@@ -15,7 +15,7 @@ export class JokeListComponent
    jokes: Joke[];
    constructor() {
        this.jokes = [
-           new Joke('Joke1', 'Me 1'),
+           new Joke('Joke1', '111 1'),
            new Joke('Joke2', 'Hello 2'),
            new Joke('Joke3', 'Hello 3')
        ];
