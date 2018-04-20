ac6a3e7a263296946de8059a9c10547f3b59bc5a
JasleneKong <jas_xueping@hotmail.com>
2018-04-20 15:45:44 +0800

    Routerupdate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+
+import { RouterModule, Routes } from '@angular/router';
+
 import { JokeListComponent } from './joke-list/joke-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -8,16 +11,27 @@ import { AppComponent } from './app.component';
 import { JokeComponent } from './joke/joke.component';
 
 import { JokeFormComponent } from './joke-form/joke-form.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'jokes', component: JokeListComponent},
+  { path: 'joke-list', redirectTo: 'jokes'},
+  { path: '**', component: PageNotFoundComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     JokeComponent,
     JokeListComponent,
-    JokeFormComponent
+    JokeFormComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
